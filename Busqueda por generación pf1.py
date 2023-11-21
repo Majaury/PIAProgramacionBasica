b554a0e74eb0616116a786f44f5270bb7d9ceb46
Majaury <151496269+Majaury@users.noreply.github.com>
2023-11-20 20:18:42 -0600

    Update Busqueda por generación pf1.py

--- Busqueda por generación pf1.py
+++ Busqueda por generación pf1.py
@@ -33,13 +33,48 @@ if os.path.exists(ubicación_archivo):
     #Cargar datos del Excel 
     excel_datos = openpyxl.load_workbook(ubicación_archivo)
     hoja_1 = excel_datos.active 
-    generación = list() 
-    cantidad = list() 
-    for i in range (1, 10, 1):
-        generación.append(hoja_1.cell(row = i+1, column = 2).value)
-        cantidad.append(hoja_1.cell(row = i+1, column = 3).value)   
-    print (generación) 
-    print (cantidad) 
+    seguro = '100%'
+    if verificación == seguro:
+        verificación = hoja_1['D11'].value
+        generación = list() 
+        cantidad = list() 
+        for i in range (1, 10, 1):
+            generación.append(hoja_1.cell(row = i+1, column = 2).value)
+            cantidad.append(hoja_1.cell(row = i+1, column = 3).value)   
+        print (generación) 
+        print (cantidad)
+    else:
+        try:    
+            excel_datos = openpyxl.Workbook()
+            #hoja_2 = excel_datos.create_sheet('Hoja_2', 1)
+            hoja_1 = excel_datos.active
+            hoja_1.title = 'Busqueda por generación'
+            #hoja_2.title = 'Busqueda por tipo'
+            a = hoja_1.cell(row = 11, column = 1, value = 'Total')
+            b = hoja_1.cell(row = 1, column = 2, value = 'Generación')
+            c = hoja_1.cell(row = 1, column = 3, value = 'Cantidad')
+            d = hoja_1.cell(row = 1, column = 4, value = 'Porcentaje')
+            Numero_pokemones_por_genera = list()
+            for i in range(1,10,1):
+                j = str(i)
+                pokemon_data_url = (url_api_gene + j)
+                data = getdata(pokemon_data_url)
+                Numero_pokemones_por_genera.append(len(data['pokemon_species']))
+                e = hoja_1.cell(row = i+1, column = 2, value = i)
+                f = hoja_1.cell(row = i+1, column = 3, value = len(data['pokemon_species']))
+            #print (Numero_pokemones_por_genera)
+            total = sum(Numero_pokemones_por_genera)
+            h = hoja_1.cell(row = 11, column = 3, value = total)
+            l = hoja_1.cell(row = 11, column = 4, value = '100%')
+            for i in range (len(Numero_pokemones_por_genera)):
+                w = promedio_2(Numero_pokemones_por_genera[i], total)
+                g = hoja_1.cell(row = i+2, column = 4, value = w)
+            excel_datos.save('Excel datos.xlsx')
+            print (Numero_pokemones_por_genera)
+        except(requests.exceptions.RequestException, OSError) as e:
+            print ('No se a podido conectar con la API, verifique su conexión a internet')
+
+        
 else:
     try:    
         excel_datos = openpyxl.Workbook()
